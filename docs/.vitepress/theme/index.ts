c5212f3145c13f6ae02bc640a5f81218d4de3ee0
cpf <397649079@qq.com>
2026-02-18 22:23:45 +0800

    style: switch Chinese font from Noto Sans SC to HarmonyOS Sans SC and fix loop arrow direction
    Replace Noto Sans SC with HarmonyOS Sans SC for better visual harmony
    with the geometric English fonts (Space Grotesk / DM Sans). The new font
    is loaded via pre-split npm package with unicode-range for on-demand
    loading. Also fix the recursive loop arrow in context-supply-chain.svg
    which was pointing right instead of left.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './layout'
+import 'harmonyos-sans-sc-webfont-splitted'
 import './style.css'
 
 const theme: Theme = {
